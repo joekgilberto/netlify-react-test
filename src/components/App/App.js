0e58e3bcae10cbabe42739b5588ce8624faf8463
joegilberto <joekgilberto@gmail.com>
2023-08-22 13:48:52 -0400

    part 3 dev skills complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import SkillList from './SkillList';
 import NewSkillForm from './NewSkillForm';
 
 function App() {
 
-  const skills = [
+  const [skills, setSkills] = useState([
     {name: "HTML", level: 5},
     {name: "CSS", level: 3},
     {name: "JavaScript", level: 4},
     {name: "Python", level: 2}
-  ]
+  ])
 
   return (
     <div className="App">
