6421f7f3852b9446b950bf04f0131be1952ebfd3
joegilberto <joekgilberto@gmail.com>
2023-08-23 15:41:57 -0400

    adds keys to SkillList.js

--- src/components/App/SkillList.js
+++ src/components/App/SkillList.js
@@ -3,7 +3,7 @@ import SkillListItem from './SkillListItem'
 
 export default function SkillList({skills}) {
 
-    const skillsArr = skills.map(s => <SkillListItem skill={s} />)
+    const skillsArr = skills.map(s => <SkillListItem skill={s} key={s.name.slice(0,3)} />)
     return (
         <div className="list">
             {skillsArr}
